454584cdd6f7ba7bfb374a6584a719360567e5b2
Danilo Bandeira <danilobandeira29@gmail.com>
2024-11-10 19:10:30 -0300

    feat: configured slog, created producer and publish a transaction message

--- cmd/producer/main.go
+++ cmd/producer/main.go
@@ -2,30 +2,88 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"github.com/confluentinc/confluent-kafka-go/v2/kafka"
-	"log"
+	"log/slog"
+	"os"
 )
 
+type Transaction struct {
+	Value  float64 `json:"value"`
+	FromId string  `json:"from_id"`
+	ToId   string  `json:"to_id"`
+}
+
 func init() {
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
+	slog.SetDefault(logger)
 	adminClient, err := kafka.NewAdminClient(&kafka.ConfigMap{
-		"bootstrap.server": "kafka-topics-kafka-1:9092",
+		"bootstrap.servers": "kafka:9092",
 	})
 	if err != nil {
-		log.Fatalf("cannot start kafka's admin client: %v\n", adminClient)
+		slog.Error("cannot start kafka's admin client\n",
+			slog.String("context", "init"),
+			slog.String("error", err.Error()),
+		)
+		os.Exit(1)
 	}
 	defer adminClient.Close()
 	_, err = adminClient.CreateTopics(context.Background(), []kafka.TopicSpecification{
 		{
 			Topic:             "balance-updated",
 			NumPartitions:     3,
-			ReplicationFactor: 2,
+			ReplicationFactor: 1,
 		},
 	})
 	if err != nil {
-		log.Fatalf("cannot create topic %v\n", err)
+		slog.Error("cannot create topic \n",
+			slog.String("context", "init"),
+			slog.String("error", err.Error()),
+		)
+		os.Exit(1)
 	}
+	slog.Info("kafka connected and topic 'balance-updated' created")
 }
 
 func main() {
-
+	producer, err := kafka.NewProducer(&kafka.ConfigMap{
+		"bootstrap.servers": "kafka:9092",
+	})
+	if err != nil {
+		slog.Error("cannot create producer",
+			slog.String("context", "createproducer"),
+			slog.String("error", err.Error()),
+		)
+		os.Exit(1)
+	}
+	defer producer.Close()
+	topic := "balance-updated"
+	jsonBytes, err := json.Marshal(&Transaction{
+		Value:  40.0,
+		FromId: "1",
+		ToId:   "22",
+	})
+	if err != nil {
+		slog.Error("when trying to marshal Transaction",
+			slog.String("context", "createmessage"),
+			slog.String("error", err.Error()),
+		)
+		return
+	}
+	err = producer.Produce(&kafka.Message{
+		Value: jsonBytes,
+		Key:   nil,
+		TopicPartition: kafka.TopicPartition{
+			Topic:     &topic,
+			Partition: kafka.PartitionAny,
+		},
+	}, nil)
+	if err != nil {
+		slog.Error("cannot produce a message",
+			slog.String("context", "createmessage"),
+			slog.String("error", err.Error()),
+		)
+		os.Exit(1)
+	}
+	producer.Flush(1000)
 }
--- readme.md
+++ readme.md
@@ -1,6 +1,5 @@
-# Running the Project
+## Running the application
 
 ```shell
-$ docker build -t kafka-topics
-$ docker run --rm --name kafka -v $(pwd):/go/src -v /go/src/tmp -e AIR_TMP_DIR=/go/src/tmp kafka-topics
+$ docker compose up --build
 ```
